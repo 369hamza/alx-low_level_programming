6ae517f882f01c06a1abc7d7ba21d77c02d97b6e
369hamza <el.ouardi.hamza369@gmail.com>
2024-05-20 23:30:09 -0700

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,12 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints a specific message to standard error
+ *
+ * Return: Always 1 (Error code)
+ */
 int main(void)
 {
 	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
