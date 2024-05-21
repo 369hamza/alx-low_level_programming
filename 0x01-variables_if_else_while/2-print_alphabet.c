447b015e81f1c701e3ec308b652e943d07b30f1d
369hamza <el.ouardi.hamza369@gmail.com>
2024-05-20 23:47:14 -0700

    fix

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,19 +2,18 @@
 #include <ctype.h>
 
 /**
-*main - Program to print alphabet letter followed by new line
-*
-*Return: return 0
-*/
+ * main - Program to print alphabet letter followed by new line
+ *
+ * Return: return 0
+ */
 int main(void)
 {
-	int letter;
-	
+       	int letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-		letter = tolower(letter);
-		putchar(letter);
-	}
+    {
+	    putchar(letter);
+    }
 	putchar('\n');
 	return (0);
 }
